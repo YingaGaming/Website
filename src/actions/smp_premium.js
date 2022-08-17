a4a3ab0c8698cc3f0b182800b785f44d9b41d006
Xenorio <hi@xenorio.xyz>
2022-08-17 17:02:51 +0200

    add notification

--- src/actions/smp_premium.js
+++ src/actions/smp_premium.js
@@ -2,4 +2,5 @@ const rcon = require('../util/rcon')
 
 module.exports.run = (order) => {
     rcon.client.executeCommand(`lp user ${order.username} parent add premium`)
+    rcon.client.executeCommand(`tellraw @a ["",{"text":"[","color":"dark_aqua"},{"text":"Shop","bold":true,"color":"gold"},{"text":"] ","color":"dark_aqua"},{"text":"${order.username} ","bold":true,"color":"light_purple"},{"text":"ist jetzt Premium!","color":"gold"}]`)
 }
