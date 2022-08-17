1e81d4b0e1331faa0fc39558dd25843bc51a8820
Xenorio <hi@xenorio.xyz>
2022-08-17 16:56:37 +0200

    reconnect rcon

--- src/util/rcon.js
+++ src/util/rcon.js
@@ -3,6 +3,10 @@ const config = require('../../config')
 
 const client = new RCONClient(config.rcon.host, config.rcon.password, config.rcon.port);
 
+client.on('disconnect', () => {
+    client.connect()
+})
+
 module.exports.connect = () => {
     client.connect();
     console.log('RCON Connected')
