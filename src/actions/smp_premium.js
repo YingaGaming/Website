a659505e731f177313ef5972b29495ce91b50ea7
Xenorio <hi@xenorio.xyz>
2022-08-20 00:24:18 +0200

    re-enable giving ranks

--- src/actions/smp_premium.js
+++ src/actions/smp_premium.js
@@ -2,7 +2,6 @@ const rcon = require('../util/rcon')
 const mongo = require('../util/mongo')
 
 module.exports.run = (order) => {
-    return
     rcon.client.executeCommand(`lp user ${order.username} parent add premium`)
     rcon.client.executeCommand(`tellraw @a ["",{"text":"[","color":"dark_aqua"},{"text":"Shop","bold":true,"color":"gold"},{"text":"] ","color":"dark_aqua"},{"text":"${order.username} ","bold":true,"color":"light_purple"},{"text":"ist jetzt Premium!","color":"gold"}]`)
 
